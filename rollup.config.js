994bbbcdf9a6f7eef2ca988ebf2aeaf66567f641
Kazan-Strelnikova <59692605+Kazan-Strelnikova@users.noreply.github.com>
2024-06-30 16:57:12 +0000

    Deploying to gh-pages from @ Kazan-Strelnikova/FWD@363e21d2353f2c5ce3b4d5ab3fb4fa860dcdc9ca 🚀

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+
+
+export default {
+  input: {
+    main: 'src/script.ts',
+    comic: 'src/script_comic.ts',
+  },
+  output: {
+    dir: 'dist',
+    format: 'es',
+    entryFileNames: '[name].bundle.js',
+    chunkFileNames: '[name]-[hash].js',
+  },
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+    typescript({ tsconfig: './tsconfig.json' }),
+    terser(),
+  ],
+};
